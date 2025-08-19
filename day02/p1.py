9aacc4d956fa2edfbc976bfd0d5d64307c03958e
PBJ <wabco0222@naver.com>
2025-08-19 10:27:03 +0900

    0819

---
+++ day02/p1.py
@@ -0,0 +1,25 @@
+
+a = 10
+
+
+if __name__ == '__main__':
+    while True:
+        cmd = input("명령을 입력하세요(p,m,q)")
+        if cmd == "p":
+            print("plus..")
+            num1 = int(input("INPUT NUM1 : "))
+            num2 = int(input("INPUT NUM2 : "))
+            result = num1 + num2
+            print(f"plus 결과는 : {result}")
+
+        elif cmd == "m":
+            print("minus.")
+            num1 = int(input("INPUT NUM1 : "))
+            num2 = int(input("INPUT NUM2 : "))
+            result = num1 - num2
+            print(f"minus 결과는 : {result}")
+        elif cmd == "q":
+            print("bye")
+            break
+
+
